72be756041d5f939bbd6321fbca9d3320ae34532
unknown <namelost@protonmail.com>
2018-12-19 13:32:51 +0800

    IIFE included

--- jsinfo/ch15/scripts/test.js
+++ jsinfo/ch15/scripts/test.js
@@ -1,20 +1,21 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
-const ask = function ask(question, yes, no) {
+(function IIFE() {
+  /* eslint-disable no-restricted-globals */
+  /* eslint-disable no-alert */
+  /* eslint-disable no-console */
+  const ask = function ask(question, yes, no) {
   // eslint-disable-next-line no-undef
-  if (confirm(question)) return yes();
-  return no();
-};
-// const showOk = function showOk() {
-//   return 'You agreed';
-// };
-// const showCancel = function showCancel() {
-//   return 'You cancelled the execution.';
-// };
-
-console.log(
-  ask('Do you agree?',
-    () => 'You agreed',
-    () => 'You cancelled the execution.'),
-);
+    if (confirm(question)) return yes();
+    return no();
+  };
+  // const showOk = function showOk() {
+  //   return 'You agreed';
+  // };
+  // const showCancel = function showCancel() {
+  //   return 'You cancelled the execution.';
+  // };
+  console.log(
+    ask('Do you agree?',
+      () => 'You agreed',
+      () => 'You cancelled the execution.'),
+  );
+}());
