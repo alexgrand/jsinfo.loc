8a44fb490869f14fb5641ea704ada3daf3cc167e
unknown <namelost@protonmail.com>
2018-12-24 17:36:56 +0800

    added modules for func from app.js and pass test to mocha

--- jsinfo/Code quality/Mocha testing/mocha test part 2/temperature/app.js
+++ jsinfo/Code quality/Mocha testing/mocha test part 2/temperature/app.js
@@ -1,11 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-const celsiusToFahrenheit = function cToF(celsius) {
+const convert = {};
+convert.celsiusToFahrenheit = function cToF(celsius) {
   if (!Number.isInteger(celsius)) return undefined;
   return celsius * 9 / 5 + 32;
 };
 
 // eslint-disable-next-line no-unused-vars
-const fahrenheitToCelsius = function fToC(fahrenheit) {
+convert.fahrenheitToCelsius = function fToC(fahrenheit) {
   if (!Number.isInteger(fahrenheit)) return undefined;
   return (fahrenheit - 32) * 5 / 9;
 };
+
+module.exports = convert;
--- jsinfo/Code quality/Mocha testing/mocha test part 2/temperature/test/test.js
+++ jsinfo/Code quality/Mocha testing/mocha test part 2/temperature/test/test.js
@@ -1,17 +1,28 @@
+/* eslint-disable no-undef */
 const assert = require('assert');
+const convert = require('../app.js');
 
-// eslint-disable-next-line no-undef
 describe('Temperature conversation', () => {
-  // eslint-disable-next-line no-undef
   describe('celsiusToFahrenheit', () => {
-    // eslint-disable-next-line no-undef
     it('should convert -40 celsius to -40 fahrenheit', () => {
-      // eslint-disable-next-line no-undef
-      assert.equal(-40, celsiusToFahrenheit(-40));
+      assert.equal(-40, convert.celsiusToFahrenheit(-40));
+    });
+    it('should convert 0 celsius to 32 fahrenheit', () => {
+      assert.equal(32, convert.celsiusToFahrenheit(0));
+    });
+    it('should return undefined if no temperature is input', () => {
+      assert.equal(undefined, convert.celsiusToFahrenheit(''));
     });
   });
-  // eslint-disable-next-line no-undef
   describe('fahrenheitToCelsius', () => {
-
+    it('should convert -40 fahrenheit to -40 celsius', () => {
+      assert.equal(-40, convert.fahrenheitToCelsius(-40));
+    });
+    it('should convert 32 fahrenheit to 0 celsius', () => {
+      assert.equal(0, convert.fahrenheitToCelsius(32));
+    });
+    it('should return undefined if no temperature is input', () => {
+      assert.equal(undefined, convert.fahrenheitToCelsius(''));
+    });
   });
 });
