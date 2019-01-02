69d2e9726dd8a741354c3ccad6fa0b2645ee0f02
unknown <namelost@protonmail.com>
2019-01-02 14:37:10 +0800

    added 'time execution check' for v1 and v2 of isEmpty()

--- jsinfo/Objects/ch01 Objects/scripts/checkForEmptiness.js
+++ jsinfo/Objects/ch01 Objects/scripts/checkForEmptiness.js
@@ -3,9 +3,20 @@ Write the function isEmpty(obj) which returns true if the object has no properti
 */
 
 const schedule = {};
+// need to be checked for Performance v1 and v2
 const isEmpty = function hasProperty(obj) {
+  // v1
   return !Object.keys(obj).length;
+
+  // v2
+  // for (const key in obj) {
+  //   return false;
+  // }
+  // return true;
 };
 console.log(isEmpty(schedule));
 schedule['8:30'] = 'get up';
+
+console.time('isEmpty');
 console.log(isEmpty(schedule));
+console.timeEnd('isEmpty');
